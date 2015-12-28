9611b9dc527076c05785605cfab67470809dc26c
luqin <luqin2006@163.com>
2015-12-28 13:36:12 +0800

    examples lib

--- examples/lib/index.js
+++ examples/lib/index.js
@@ -11,7 +11,7 @@ var _classCallCheck = require('babel-runtime/helpers/class-call-check')['default
 var _interopRequireDefault = require('babel-runtime/helpers/interop-require-default')['default'];
 
 Object.defineProperty(exports, '__esModule', {
-    value: true
+  value: true
 });
 
 var _react = require('react');
@@ -19,26 +19,26 @@ var _react = require('react');
 var _react2 = _interopRequireDefault(_react);
 
 var ReactComponent = (function (_React$Component) {
-    _inherits(ReactComponent, _React$Component);
+  _inherits(ReactComponent, _React$Component);
 
-    function ReactComponent() {
-        _classCallCheck(this, ReactComponent);
+  function ReactComponent() {
+    _classCallCheck(this, ReactComponent);
 
-        _get(Object.getPrototypeOf(ReactComponent.prototype), 'constructor', this).apply(this, arguments);
+    _get(Object.getPrototypeOf(ReactComponent.prototype), 'constructor', this).apply(this, arguments);
+  }
+
+  _createClass(ReactComponent, [{
+    key: 'render',
+    value: function render() {
+      return _react2['default'].createElement(
+        'div',
+        null,
+        'ReactComponent'
+      );
     }
+  }]);
 
-    _createClass(ReactComponent, [{
-        key: 'render',
-        value: function render() {
-            return _react2['default'].createElement(
-                'div',
-                null,
-                'ReactComponent'
-            );
-        }
-    }]);
-
-    return ReactComponent;
+  return ReactComponent;
 })(_react2['default'].Component);
 
 exports['default'] = ReactComponent;
