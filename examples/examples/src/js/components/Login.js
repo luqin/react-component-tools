6e86ccf3c5c7033bcaa7db690177b83bf00112b9
luqin <h-io@qq.com>
2016-01-07 22:48:22 +0800

    0.6.0

---
+++ examples/examples/src/js/components/Login.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+class Login extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+
+    this.handleLogin = this.handleLogin.bind(this);
+  }
+
+  componentDidMount() {
+  }
+
+  handleLogin() {
+    let data = {
+      username: this.ref.username.value,
+      pwd: this.ref.pwd.value,
+    }
+
+    $.ajax({
+      data: data
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleLogin}>
+          <input ref="username" type="text" name="username"/>
+          <input ref="pwd" type="password" name="pwd"/>
+          <input type="submit"/>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Login;
